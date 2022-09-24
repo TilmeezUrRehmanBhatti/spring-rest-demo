4048cdd86145fee03aba96771f0e4579fae1cc0b
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-24 14:11:38 +0200

    Exception Handling - Add Exception Handler with @ExceptionHandler

--- src/main/java/com/tilmeez/springdemo/rest/StudentRestController.java
+++ src/main/java/com/tilmeez/springdemo/rest/StudentRestController.java
@@ -1,10 +1,9 @@
 package com.tilmeez.springdemo.rest;
 
 import com.tilmeez.springdemo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -50,6 +49,23 @@ public class StudentRestController {
         }
 
         return theStudents.get(studentId);
+    }
+
+    // Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+
+        // create a StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
 }
